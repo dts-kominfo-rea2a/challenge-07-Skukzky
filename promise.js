45d70107037a12e96a9f7e5cf6be443bbe330fbf
Skukzky <rifkioskar1@gmail.com>
2022-06-12 08:25:27 +0700

    Tantangan: Promise

--- promise.js
+++ promise.js
@@ -1,7 +1,19 @@
 const { promiseTheaterIXX, promiseTheaterVGC } = require("./external.js");
 
 // TODO: Buat fungsi promiseOutput sesuai ketentuan readme
-const promiseOutput = null;
+const promiseOutput = async (emotion) => {
+  try{
+    let sumEmotion = 0
+    let dataIXX = await promiseTheaterIXX();
+    let dataVGC = await promiseTheaterVGC();
+    const data = dataIXX.concat(dataVGC);
+
+    data.map(dataFilm => dataFilm.hasil === emotion && (sumEmotion += 1))
+    return sumEmotion;
+  } catch (err) {
+    return err
+  }
+}
 
 module.exports = {
   promiseOutput,
